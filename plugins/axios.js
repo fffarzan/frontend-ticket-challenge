521452bd314ef45201e60915970a87feee3410af
farzan nedaei <far.ned.me@gmail.com>
2021-04-05 21:54:13 +0430

    Adding actionFn to axios plugin

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,5 +1,22 @@
 import { apiUrl } from '@/utils/environment'
 
-export default ({ $axios }) => {
+export default ({ $axios }, inject) => {
+  // create base url with axios
   $axios.setBaseURL(apiUrl.baseURL)
+
+  // create a general function to handle apis
+  const actionFn = (fn) => async () => {
+    let result
+
+    try {
+      result = await fn($axios)
+    } catch (e) {
+      result = false
+    }
+
+    return result
+  }
+
+  // inject general function to nuxt instance
+  inject('actionFn', actionFn)
 }
