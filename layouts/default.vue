7d982d830c01b70755e64ac2b2456c77561ad70a
farzan nedaei <far.ned.me@gmail.com>
2021-04-06 02:44:53 +0430

    Styling the loader component

--- layouts/default.vue
+++ layouts/default.vue
@@ -63,17 +63,4 @@ a {
   text-align: center;
   line-height: 50px;
 }
-
-.loader {
-  position: absolute;
-  top: 0;
-  right: 0;
-  left: 0;
-  bottom: 0;
-  width: 100%;
-  text-align: center;
-  font-size: 30px;
-  background-color: rgba(0, 0, 0, 0.9);
-  line-height: 100vh;
-}
 </style>
