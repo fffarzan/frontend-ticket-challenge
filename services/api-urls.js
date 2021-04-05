6b0e9a45bc73bc1267fa1bd02a1ca591f943b50b
farzan nedaei <far.ned.me@gmail.com>
2021-04-06 02:17:52 +0430

    Fixing buy api

--- services/api-urls.js
+++ services/api-urls.js
@@ -2,6 +2,6 @@ export const ApiUrls = {
   map: {
     getAll: (api) => api.$get('/map'),
     get: (api, id) => api.$get(`/map/${id}`),
-    buy: (api, payload) => api.$post(`/map/${payload.mapId}/ticket`, payload),
+    buy: (api, payload) => api.$post(`/map/${payload.mapId}/ticket`),
   },
 }
